a3af3e5402cd5eaceb5753b93685da81e5986ac9
WhiteTrefoil <whitetrefoil@gmail.com>
2014-07-16 20:26:30 +0800

    app files

---
+++ app.py
@@ -0,0 +1,22 @@
+# !/usr/bin/env python
+
+
+"""helloworld.py: The main entry file of this application."""
+
+
+from eve import Eve
+
+
+__author__ = "WhiteTrefoil"
+__credits__ = ["WhiteTrefoil"]
+__version__ = "0.0.0"
+__maintainer__ = "WhiteTrefoil"
+__email__ = "whitetrefoil@gmail.com"
+__status__ = "Prototype"
+
+
+app = Eve()
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ settings.py
@@ -0,0 +1,27 @@
+# !/usr/bin/env python
+
+
+"""settings.py: The settings file of Eve"""
+
+
+__author__ = "WhiteTrefoil"
+__credits__ = ["WhiteTrefoil"]
+__version__ = "0.0.0"
+__maintainer__ = "WhiteTrefoil"
+__email__ = "whitetrefoil@gmail.com"
+__status__ = "Prototype"
+
+
+# API Config
+DOMAIN = {}
+
+# DB Config
+MONGO_HOST = 'localhost'
+
+# Resource Config
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+
+if __name__ == '__main__':
+    pass
